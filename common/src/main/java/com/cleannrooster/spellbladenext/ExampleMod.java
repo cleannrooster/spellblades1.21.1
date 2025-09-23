8c39303dbbb1ccf8aa8d7504d9101c7b3f3664fa
cleannrooster <95945095+cleannrooster@users.noreply.github.com>
2025-09-23 08:07:49 -0700

    setup

---
+++ common/src/main/java/com/cleannrooster/spellbladenext/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.cleannrooster.spellbladenext;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "spellbladenext";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/cleannrooster/spellbladenext/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.cleannrooster.spellbladenext.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.cleannrooster.spellbladenext.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
